047a5d7bbed8257389531e50ebd3910c521e9dcd
zhanghaojie <1455123087@qq.com>
2024-05-26 09:00:07 +0800

    chore: Update dependencies and add counter store
    This commit updates the dependencies in the `package.json` file and adds a new counter store in the `src/stores/counter.ts` file. The counter store uses Zustand to manage the count state and provides an `increment` function to increment the count value.

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,15 @@
-import { Text, View } from "@tarojs/components";
-
+import { useCounterStore } from "@/stores/counter";
+import { Button, Text, View } from "@tarojs/components";
 import "./index.less";
 import styles from "./styles.module.less";
 
 const Index = () => {
+  const count = useCounterStore((store) => store.count);
+  const increment = useCounterStore((store) => store.increment);
   return (
     <View className="wrapper">
       <Text className={styles.title}>Hi</Text>
+      <Button onClick={() => increment()}>{count}</Button>
     </View>
   );
 };
