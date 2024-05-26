8464cb3aa59be4b0aac2e95a4c9fa262e9db22ac
zhanghaojie <1455123087@qq.com>
2024-05-26 10:06:03 +0800

    chore: Update ESLint and Prettier configurations
    This commit updates the ESLint configuration in the ".eslintrc.js" file to extend the "taro/react" preset and adds the "@typescript-eslint/parser" as the parser. It also updates the Prettier configuration in the ".prettierrc" file to include various formatting rules. These changes improve the code quality and maintainability of the project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  extends: ['taro/react'],
+  extends: ["taro/react"],
+  parser: "@typescript-eslint/parser",
   rules: {
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "import/no-commonjs": "off",
+    quotes: ["warn", "double"],
+    "jsx-quotes": ["warn", "prefer-double"],
   },
 };
